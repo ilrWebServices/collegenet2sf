02b77b82536362a6989c301a148259911127ca4e
Jeff Amaral <jeff.amaral@cornell.edu>
2024-01-08 19:11:03 -0500

    Update collegenet2sf:run (cnet2sf) command for drush 12 compatibility

--- src/Drush/Commands/Collegenet2sfCommands.php
+++ src/Drush/Commands/Collegenet2sfCommands.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace Drupal\collegenet2sf\Commands;
+namespace Drupal\collegenet2sf\Drush\Commands;
 
 use Consolidation\AnnotatedCommand\Input\StdinAwareInterface;
 use Consolidation\AnnotatedCommand\Input\StdinAwareTrait;
 use Drush\Commands\DrushCommands;
+use Drush\Attributes as CLI;
 use Drupal\collegenet2sf\CollegeNetToSalesforceProcessor;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Drush commandfile for collegenet2sf.
@@ -14,21 +16,29 @@ class Collegenet2sfCommands extends DrushCommands implements StdinAwareInterface
 
   use StdinAwareTrait;
 
+  /**
+   * Constructs an Collegenet2sfCommands object.
+   */
+  public function __construct(
+    private readonly CollegeNetToSalesforceProcessor $collegenet2sfProcessor
+  ) {
+    parent::__construct();
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function __construct(CollegeNetToSalesforceProcessor $collegenet2sf_processor) {
-    parent::__construct();
-    $this->collegenet2sfProcessor = $collegenet2sf_processor;
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('collegenet2sf.processor'),
+    );
   }
 
   /**
    * Run the CollegeNET to Salesforce Lead processor.
-   *
-   * @command collegenet2sf:run
-   * @aliases cnet2sf
    */
+  #[CLI\Command(name: 'collegenet2sf:run', aliases: ['cnet2sf'])]
+  #[CLI\Usage(name: 'collegenet2sf:run', description: 'Run the Collegenet to Salesforce Lead processor')]
   public function run(array $extra) {
     $command = array_shift($extra);
     $data = ($command === '-') ? $this->stdin()->contents() : NULL;
